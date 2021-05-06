74e4711c4a61caa7acf094656294ad9dba9ebea9
ste-ioan <72309874+ste-ioan@users.noreply.github.com>
2021-05-06 14:54:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -43,24 +43,15 @@ wines.reset_index(inplace=True, drop=True)
 wines['Anise'] = (wines['Anise'].fillna(0) + wines['Star anise'].fillna(0)).replace({'0':np.nan, 0:np.nan})
 wines['Tropical'] = (wines['Tropical'].fillna(0) + wines['Mango'].fillna(0) + wines['Pineapple'].fillna(0) + wines['Green papaya'].fillna(0) +
                      wines['Passion fruit'].fillna(0) + wines['Guava'].fillna(0) + wines['Green mango'].fillna(0)).replace({'0':np.nan, 0:np.nan})
-wines['Lemon'] = (wines['Lemon'].fillna(0) + wines['Citrus'].fillna(0)).replace({'0':np.nan, 0:np.nan})
-wines['Orange'] = (wines['Orange'].fillna(0) + wines['Blood orange'].fillna(0) + wines['Citrus'].fillna(0)).replace({'0':np.nan, 0:np.nan})
-wines['Tangerine'] = (wines['Tangerine'].fillna(0) + wines['Citrus'].fillna(0)).replace({'0':np.nan, 0:np.nan})
-wines['Grapefruit'] = (wines['Grapefruit'].fillna(0) + wines['Citrus'].fillna(0)).replace({'0':np.nan, 0:np.nan})
-wines['Lime'] = (wines['Lime'].fillna(0) + wines['Citrus'].fillna(0)).replace({'0':np.nan, 0:np.nan})
+wines['Orange'] = (wines['Orange'].fillna(0) + wines['Blood orange'].fillna(0)).replace({'0':np.nan, 0:np.nan})
 wines['Smoke'] = (wines['Smoke'].fillna(0) + wines['Campfire'].fillna(0)).replace({'0':np.nan, 0:np.nan})
 wines['Orange zest'] = (wines['Orange zest'].fillna(0) + wines['Orange peel'].fillna(0) + wines['Orange rind'].fillna(0)).replace({'0':np.nan, 0:np.nan})
 wines['Dark fruit'] = (wines['Dark fruit'].fillna(0) + wines['Black fruit'].fillna(0)).replace({'0':np.nan, 0:np.nan})
-wines['Almond'] = (wines['Almond'].fillna(0) + wines['Nutty'].fillna(0)).replace({'0':np.nan, 0:np.nan})
-wines['Walnut'] = (wines['Walnut'].fillna(0) + wines['Nutty'].fillna(0)).replace({'0':np.nan, 0:np.nan})
-wines['Macadamia nut'] = (wines['Macadamia nut'].fillna(0) + wines['Nutty'].fillna(0)).replace({'0':np.nan, 0:np.nan})
-wines['Hazelnut'] = (wines['Hazelnut'].fillna(0) + wines['Nutty'].fillna(0)).replace({'0':np.nan, 0:np.nan})
-wines['Potpourri'] =  (wines['Potpourri'].fillna(0) + wines['Dried rose'].fillna(0) + wines['Dried flowers'].fillna(0)).replace({'0':np.nan, 0:np.nan})
+wines['Dried flowers'] =  (wines['Dried flowers'].fillna(0) + wines['Dried rose'].fillna(0) + wines['Potpourri'].fillna(0)).replace({'0':np.nan, 0:np.nan})
 
 
-
-wines.drop(['Star anise', 'Mango', 'Pineapple', 'Passion fruit', 'Citrus', 'Orange peel', 'Guava', 'Green mango',
-            'Green papaya', 'Orange rind', 'Blood orange', 'Campfire', 'Black fruit', 'Nutty', 'Dried rose', 'Dried flowers'], axis=1, inplace=True)
+wines.drop(['Star anise', 'Mango', 'Pineapple', 'Passion fruit', 'Orange peel', 'Guava', 'Green mango',
+            'Green papaya', 'Orange rind', 'Blood orange', 'Campfire', 'Black fruit', 'Dried rose', 'Potpourri'], axis=1, inplace=True)
 
 # find the rare taste features (in less than 10 percent of the sample), drop them from the df
 rare_features = wines.isnull().sum(axis=0)[wines.isnull().sum(axis=0) > (wines.shape[0] * 0.90)].index.to_list()
